2813cf896a70b2b60496bbec0f1eee426d17eb7b
Tracy Fussell <tracy.fussell@gmail.com>
2018-10-24 20:22:15 -0400

    Addition of Read.Me file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-
 class Book extends React.Component{
-
 
   render(){
     return(
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './index.css'
 
 
 import {BrowserRouter} from 'react-router-dom'
-
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
